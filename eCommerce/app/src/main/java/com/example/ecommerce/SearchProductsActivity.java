333938cbcfd752f6dc509050a76c5e34ed0fec80
congchinh <chinhngocong2308@gmail.com>
2021-05-16 08:15:52 +0700

    four commit

--- eCommerce/app/src/main/java/com/example/ecommerce/SearchProductsActivity.java
+++ eCommerce/app/src/main/java/com/example/ecommerce/SearchProductsActivity.java
@@ -1,19 +1,33 @@
 package com.example.ecommerce;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+
+import com.example.ecommerce.Model.Products;
+import com.example.ecommerce.ViewHolder.ProductViewHolder;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.squareup.picasso.Picasso;
 
 public class SearchProductsActivity extends AppCompatActivity {
 
     private ImageView SearchBtn;
     private EditText inputText;
     private RecyclerView searchList;
+    private String SearchInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,5 +39,59 @@ public class SearchProductsActivity extends AppCompatActivity {
         searchList = findViewById(R.id.search_list);
 
         searchList.setLayoutManager(new LinearLayoutManager(SearchProductsActivity.this));
+
+        SearchBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SearchInput = inputText.getText().toString();
+
+                onStart();
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference().child("Products");
+
+        //Search flow name product
+        FirebaseRecyclerOptions<Products> options = new FirebaseRecyclerOptions.Builder<Products>()
+                .setQuery(reference.orderByChild("pname").startAt(SearchInput), Products.class)
+                .build();
+
+        FirebaseRecyclerAdapter<Products, ProductViewHolder> adapter = new FirebaseRecyclerAdapter<Products, ProductViewHolder>(options) {
+            @Override
+            protected void onBindViewHolder(@NonNull ProductViewHolder holder, int i, @NonNull Products products) {
+                holder.txtProductName.setText(products.getPname());
+                holder.txtProductDescription.setText(products.getDescription());
+//                        String price = model.getPrice();
+//                        holder.txtProductPrice.setText("Giá" + NumberFormat.getCurrencyInstance(new Locale("en", "US"))
+//                                .format(price) + "VNĐ");
+                holder.txtProductPrice.setText("Giá: " + products.getPrice() + "đ");
+                Picasso.get().load(products.getImage()).into(holder.imageView);
+
+
+                holder.itemView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent(SearchProductsActivity.this, ProductDetailsActivity.class);
+                        intent.putExtra("pid", products.getPid());
+                        startActivity(intent);
+                    }
+                });
+            }
+
+            @NonNull
+            @Override
+            public ProductViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.products_items_layout, parent, false);
+                ProductViewHolder holder = new ProductViewHolder(view);
+                return holder;
+            }
+        };
+        searchList.setAdapter(adapter);
+        adapter.startListening();
     }
 }
